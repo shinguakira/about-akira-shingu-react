da482e02cc7c520bdd7f4c2fca2bf6b6bad10891
AkiraShingu <shinguakira1022@gmail.com>
2024-12-14 17:39:49 +0900

    Add download-link component replace 履歴書、職務履歴書 link to this component above

---
+++ components/ui/download-link.tsx
@@ -0,0 +1,36 @@
+import React, { memo } from "react";
+import { LinkArrow } from "@/components/ui/icons";
+import Link from "next/link";
+
+type DownLoadLinkProps = {
+  href: string; // link to the page
+  target?: string; // target for the link Optional
+  className?: string; // custom styles for the link Optional
+  download?: boolean; // flag if download is true Optional
+  label: string; // label for the link
+};
+const DownLoadLink: React.FC<DownLoadLinkProps> = memo(
+  ({
+    href = "/",
+    target = "_blank",
+    className = "",
+    download = true,
+    label = "",
+  }) => {
+    return (
+      <Link
+        href={href}
+        target={target}
+        className={`${className} inline-flex items-center rounded-lg border-transparent bg-black p-1 px-1 text-sm font-semibold text-white hover:border-black`}
+        download={download}
+      >
+        {/* TODO may should separate className variables.
+        because style of Link and one of LinkArrow should be different */}
+        <LinkArrow className={`ml-2 ${className}`} />
+        {label}
+      </Link>
+    );
+  }
+);
+
+export default DownLoadLink;

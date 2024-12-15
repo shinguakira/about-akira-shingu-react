3d5f5dbd2ed61475e4167abe8a175b0d6eb3ef94
AkiraShingu <shinguakira1022@gmail.com>
2024-12-15 21:57:29 +0900

    create education-item component

--- components/ui/education-history.tsx
+++ components/ui/education-history.tsx
@@ -1,4 +1,5 @@
 import { educationHistory } from "@/constants/education-history";
+import EducationItem from "./education-item";
 type EducationHistoryProps = {
   school: string;
   department: string;
@@ -16,29 +17,15 @@ const EducationHistory: React.FC = () => {
             Education
           </h1>
           <div className="space-y-6">
-            {educationHistory.map((education) => (
-              <div className="overflow bg-white shadow-md dark:bg-gray-800 sm:rounded-lg">
-                <div className="px-4 py-5 sm:px-6">
-                  <h3 className="text-lg font-medium leading-6 text-gray-900 dark:text-white">
-                    {education.school}
-                  </h3>
-                  <p className="mt-1 max-w-2xl text-sm text-gray-500 dark:text-gray-400">
-                    {education.startYear} - {education.endYear}
-                  </p>
-                </div>
-                <div className="border-t border-gray-200 dark:border-gray-700">
-                  <dl>
-                    <div className="bg-gray-50 px-4 py-5 dark:bg-gray-700 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-                      <dt className="text-sm font-medium text-gray-500 dark:text-gray-400">
-                        Description
-                      </dt>
-                      <dd className="mt-1 text-sm text-gray-900 dark:text-white sm:col-span-2 sm:mt-0">
-                        {education.description}
-                      </dd>
-                    </div>
-                  </dl>
-                </div>
-              </div>
+            {educationHistory.map((education, index) => (
+              <EducationItem
+                key={index}
+                school={education.school}
+                department={education.department}
+                startYear={education.startYear}
+                endYear={education.endYear}
+                description={education.description}
+              />
             ))}
           </div>
         </div>

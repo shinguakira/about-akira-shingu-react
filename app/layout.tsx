2e3e8f6cafcaefcf6815992f348cea8fc83daa78
AkiraShingu <shinguakira1022@gmail.com>
2025-01-25 12:42:42 +0900

    add icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,6 +32,9 @@ export default function RootLayout({
         <title>{metadata.title as string}</title>
         <meta name="description" content={metadata.description as string} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon-32x32.ico" />
+        <link rel="icon" sizes="32x32" href="/favicon-32x32.ico" />
+        <link rel="icon" sizes="16x16" href="/favicon-16x16.ico" />
       </Head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}

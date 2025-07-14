4a6557173a430a4bb51e7002fa40d0d171ef0403
AkiraShingu <shinguakira1022@gmail.com>
2025-07-14 23:06:51 +0900

    solve error for format

--- components/ui/skill-set.tsx
+++ components/ui/skill-set.tsx
@@ -69,7 +69,7 @@ const SkillSet = () => {
       <h3 className="mb-8 w-full text-center text-3xl text-blue-900 underline dark:text-white">
         {currentLang === "ja" ? "スキル" : "Skills"}
       </h3>
-      <div className="mx-auto grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4">
+      <div className="mx-auto grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4">
         {filteredSkills.map((skill) => (
           <SkillItem
             key={skill.name}
@@ -88,7 +88,7 @@ const SkillSet = () => {
           {currentLang === "ja" ? "その他のスキル" : "Other Skills"}
         </h3>
       )}
-      <div className="mx-auto grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4">
+      <div className="mx-auto grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4">
         {filteredOtherSkills.map((otherSkill) => (
           <SkillItem
             key={otherSkill.name}
